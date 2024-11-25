80184183055e60ff35b7be1c5062ece78070f94e
kohkhatori <kouki20040124hatori@gmail.com>
2024-11-25 03:16:43 -0500

    finished get_rating function

---
+++ movie_collection/utils/rating_utils.py
@@ -0,0 +1,48 @@
+import logging
+import requests
+
+# from movie_collection.utils.logger import configure_logger
+
+logger = logging.getLogger(__name__)
+# configure_logger(logger)
+
+
+def get_rating(imdb_id: str) -> float:
+    """
+    Fetches the rating of a movie from the OMDB API.
+
+    Returns:
+        float: The rating of the movie.
+
+    Raises:
+        RuntimeError: If the request to omdbapi.com fails or returns an invalid response.
+        ValueError: If the response from omdbapi.com is not a valid float.
+    """
+    url=f"https://www.omdbapi.com/?i={imdb_id}&apikey={API_KEY}"
+
+    try:
+        # Log the request to omdbapi.com
+        logger.info("Fetching data from %s", url)
+
+        response = requests.get(url, timeout=5)
+
+        # Check if the request was successful
+        response.raise_for_status()
+
+        rating = response.json()["imdbRating"]
+
+        try:
+            rating = float(rating)
+        except ValueError:
+            raise ValueError("Invalid response from omdbapi.com: %s" % rating)
+
+        logger.info("Received rating:  %.3f", rating)
+        return rating
+
+    except requests.exceptions.Timeout:
+        logger.error("Request to omdbapi.com timed out.")
+        raise RuntimeError("Request to omdbapi.com timed out.")
+
+    except requests.exceptions.RequestException as e:
+        logger.error("Request to omdbapi.com failed: %s", e)
+        raise RuntimeError("Request to omdbapi.com failed: %s" % e)
